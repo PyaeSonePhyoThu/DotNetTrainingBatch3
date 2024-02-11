191be8eafec4cdd5c045d561b3054ca3a6039697
pyaesone0193 <108206395+pyaesone0193@users.noreply.github.com>
2024-02-11 22:21:43 +0630

    Efcore donr

--- DotNetTrainingBatch3.ConsoleApp/DapperExamples/DapperExample.cs
+++ DotNetTrainingBatch3.ConsoleApp/DapperExamples/DapperExample.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DotNetTrainingBatch3.ConsoleApp.AdoDotNetExamples;
-using System.Data;
-using System.Data.SqlClient;
 using Dapper;
 using DotNetTrainingBatch3.ConsoleApp.Models;
 using System.Reflection.Metadata;
@@ -15,31 +14,22 @@ namespace DotNetTrainingBatch3.ConsoleApp.DapperExamples
     public class DapperExample
     {
         private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
-            {
-                DataSource = "MSI",
-                InitialCatalog = "TestDb",
-                UserID = "sa",
-                Password = "sasa@123"
-            };
+        {
+            DataSource = ".",
+            InitialCatalog = "TestDb",
+            UserID = "sa",
+            Password = "sasa@123"
+        };
+
         public void Read()
         {
-            
- 
             string query = @"SELECT [BlogId]
-                              ,[BlogTitle]
-                              ,[BlogAuthor]
-                              ,[BlogContent]
-                          FROM [dbo].[Tbl_Blog]";
+                  ,[BlogTitle]
+                  ,[BlogAuthor]
+                  ,[BlogContent]
+              FROM [dbo].[Tbl_Blog]";
             using IDbConnection db = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
             List<BlogModel> lst = db.Query<BlogModel>(query).ToList();
-
-
-            //foreach (DataRow dr in dt.Rows)
-            //{
-            //    Console.WriteLine("Title..." + dr["BlogTitle"]);
-            //    Console.WriteLine("Author..." + dr["BlogAuthor"]);
-            //    Console.WriteLine("Content..." + dr["BlogContent"]);
-            //}
 
             foreach (BlogModel item in lst)
             {
@@ -47,9 +37,7 @@ namespace DotNetTrainingBatch3.ConsoleApp.DapperExamples
                 Console.WriteLine(item.BlogTitle);
                 Console.WriteLine(item.BlogAuthor);
                 Console.WriteLine(item.BlogContent);
-
             }
-
         }
 
         public void Edit(int id)
@@ -84,29 +72,36 @@ namespace DotNetTrainingBatch3.ConsoleApp.DapperExamples
         public void Create(string title, string author, string content)
         {
             string query = @"INSERT INTO [dbo].[Tbl_Blog]
-                                       ([BlogTitle]
-                                       ,[BlogAuthor]
-                                       ,[BlogContent])
-                                 VALUES
-                                       (@BlogTitle,
-                                       ,@BlogAuthor
-                                       ,@BlogContent)";
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent)";
 
             BlogModel blog = new BlogModel()
             {
                 BlogTitle = title,
                 BlogAuthor = author,
-                BlogContent = content 
+                BlogContent = content
             };
 
             using IDbConnection db = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
-            int result = db.Execute(query,blog);
-            string message = result > 0 ? "Saving Successfully" : "Saving Failed";
+            int result = db.Execute(query, blog);
+            string message = result > 0 ? "Saving Successful." : "Saving Failed.";
 
             Console.WriteLine(message);
         }
+
         public void Update(int id, string title, string author, string content)
         {
+            string query = @"UPDATE [dbo].[Tbl_Blog]
+   SET [BlogTitle] = @BlogTitle
+      ,[BlogAuthor] = @BlogAuthor
+      ,[BlogContent] = @BlogContent
+ WHERE BlogId = @BlogId";
+
             BlogModel blog = new BlogModel()
             {
                 BlogId = id,
@@ -115,36 +110,27 @@ namespace DotNetTrainingBatch3.ConsoleApp.DapperExamples
                 BlogContent = content
             };
 
-            string query = @"UPDATE [dbo].[Tbl_Blog]
-                           SET [BlogTitle] = @BlogTitle
-                              ,[BlogAuthor] = @BlogAuthor
-                              ,[BlogContent] =@BlogContent
-                         WHERE BlogId = @BlogId";
             using IDbConnection db = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
             int result = db.Execute(query, blog);
-
-            string message = result > 0 ? "Update Successfully" : "Update Failed";
+            string message = result > 0 ? "Updating Successful." : "Updating Failed.";
 
             Console.WriteLine(message);
         }
-        public void Delete(int id, string title, string author, string content)
+
+        public void Delete(int id)
         {
+            string query = @"Delete From [dbo].[Tbl_Blog] WHERE BlogId = @BlogId";
+
             BlogModel blog = new BlogModel()
             {
                 BlogId = id,
-                BlogTitle = title,
-                BlogAuthor = author,
-                BlogContent = content
             };
-            string query = @"Delete From [dbo].[Tbl_Blog]
-                         WHERE BlogId = @BlogId";
 
             using IDbConnection db = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
             int result = db.Execute(query, blog);
-            string message = result > 0 ? "Update Successfully" : "Update Failed";
+            string message = result > 0 ? "Deleting Successful." : "Deleting Failed.";
 
             Console.WriteLine(message);
         }
     }
 }
-
--- DotNetTrainingBatch3.ConsoleApp/Program.cs
+++ DotNetTrainingBatch3.ConsoleApp/Program.cs
@@ -1,13 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DotNetTrainingBatch3.ConsoleApp.AdoDotNetExamples;
 using DotNetTrainingBatch3.ConsoleApp.DapperExamples;
+using DotNetTrainingBatch3.ConsoleApp.EFCoreExamples;
 using System.Data;
 using System.Data.SqlClient;
 
 Console.WriteLine("Hello, World!");
 
 
-DapperExample dapperExample = new DapperExample();
-dapperExample.Read();
-dapperExample.Create("Test", "Lee", "Loe");
-dapperExample.Read();
+EFCoreExample  eFCoreExample = new EFCoreExample();
+
+eFCoreExample.Read();
+eFCoreExample.Edit(2);
+eFCoreExample.Edit(13);
