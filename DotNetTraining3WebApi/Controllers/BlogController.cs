ec6d4cf8f3b93c978195224c42062ee61dbf6869
pyaesone0193 <108206395+pyaesone0193@users.noreply.github.com>
2024-02-16 20:10:28 +0630

    Done Web Api CRUD

---
+++ DotNetTraining3WebApi/Controllers/BlogController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using DotNetTraining3WebApi.EFCoreExamples;
+using DotNetTraining3WebApi.Models;
+using System.Reflection;
+
+namespace DotNetTraining3WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public BlogController()
+        {
+               _db = new AppDbContext();
+        }
+
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            List<BlogModel> lst = _db.Blogs.ToList();
+            return Ok(lst);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBlog(int id)
+        {
+            BlogModel? item = _db.Blogs.FirstOrDefault(item => item.BlogId == id);
+            if (item is null)
+            {
+                return NotFound("No Data Found");
+            }
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(BlogModel blog)
+        {
+            _db.Blogs.Add(blog);
+            int result = _db.SaveChanges();
+            string message = result > 0 ? "Saving success" : "Saving fail";
+            return Ok(message);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlogs(int id, BlogModel blog)
+        {
+            BlogModel? item = _db.Blogs.FirstOrDefault(item => item.BlogId == id);
+            if(item is null)
+            {
+                return NotFound("No Data Found");
+            }
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+
+            int result = _db.SaveChanges();
+            string message = result > 0 ? "Update Successful" : "Update Fail";
+            return Ok(message);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBlog(int id)
+        {
+            BlogModel? item = _db.Blogs.FirstOrDefault(item => item.BlogId == id);
+            if(item is null)
+            {
+                return NotFound("No Data Found");
+            }
+
+            _db.Blogs.Remove(item);
+            int result = _db.SaveChanges();
+            string message = result > 0 ? "Delete Successful" : "Delete Fail";
+            return Ok(message);
+        }
+     
+    }
+}
---
+++ DotNetTraining3WebApi/EFCoreExamples/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetTraining3WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DotNetTraining3WebApi.EFCoreExamples
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+            {
+                DataSource = ".",
+                InitialCatalog = "TestDb",
+                UserID = "sa",
+                Password = "sasa@123",
+                TrustServerCertificate = true
+            };
+            optionsBuilder.UseSqlServer(_sqlConnectionStringBuilder.ConnectionString);
+        }
+
+        public DbSet <BlogModel> Blogs { get; set; }
+    }
+}
