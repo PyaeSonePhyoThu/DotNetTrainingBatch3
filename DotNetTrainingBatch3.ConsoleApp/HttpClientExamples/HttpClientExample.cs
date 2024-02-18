e598dddd4318452daa5d182640087419bab6f202
pyaesone0193 <108206395+pyaesone0193@users.noreply.github.com>
2024-02-18 21:30:31 +0630

    Hello

---
+++ DotNetTrainingBatch3.ConsoleApp/HttpClientExamples/HttpClientExample.cs
@@ -0,0 +1,39 @@
+﻿using DotNetTrainingBatch3.ConsoleApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTrainingBatch3.ConsoleApp.HttpClientExamples
+{
+    public class HttpClientExample
+    {
+        public async Task Run()
+        {
+            await Read();
+        }
+
+        private async Task Read()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("");
+            if(response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(jsonStr);
+
+                List<BlogModel> lst = JsonConvert.DeserializeObject<List<BlogModel>>(jsonStr)!;
+
+                foreach(BlogModel item in lst)
+                {
+                    Console.WriteLine(item.BlogId);
+                    Console.WriteLine(item.BlogTitle);
+                    Console.WriteLine(item.BlogAuthor);
+                    Console.WriteLine(item.BlogContent);
+                }
+            }
+        }
+    }
+}
