00293336e7f1474afcbbd1def9984bb8ce12b24f
pyaesone0193 <108206395+pyaesone0193@users.noreply.github.com>
2024-03-04 23:12:54 +0630

    Editing some codes

--- DotNetTrainingBatch3.BirdWebApi/Controllers/BirdController.cs
+++ DotNetTrainingBatch3.BirdWebApi/Controllers/BirdController.cs
@@ -21,17 +21,6 @@ namespace DotNetTrainingBatch3.BirdWebApi.Controllers
                 string json = await response.Content.ReadAsStringAsync();
                 List<BirdDataModel> birds = JsonConvert.DeserializeObject<List<BirdDataModel>>(json)!;
 
-                //List<BirdViewModel> lst = birds.Select(bird => new BirdViewModel
-                //{
-                //    BirdId = bird.Id,
-                //    BirdName = bird.BirdMyanmarName,
-                //    Desp = bird.Description,
-                //    PhotoUrl = $"https://burma-project-ideas.vercel.app/{bird.ImagePath}"
-                //}).ToList();
-                //List<BirdViewModel> lst = birds.Select(bird => Change(bird)).ToList();
-
-                //List<BirdViewModel> lst = birds.Select(bird => Change(bird)).ToList();
-
                 List<BirdViewModel> lst = new List<BirdViewModel>();
                 foreach (var bird in birds)
                 {
@@ -57,13 +46,6 @@ namespace DotNetTrainingBatch3.BirdWebApi.Controllers
                 string json = await response.Content.ReadAsStringAsync();
                 BirdDataModel bird = JsonConvert.DeserializeObject<BirdDataModel>(json)!;
 
-                //var item = new BirdViewModel
-                //{
-                //    BirdId = bird.Id,
-                //    BirdName = bird.BirdMyanmarName,
-                //    Desp = bird.Description,
-                //    PhotoUrl = $"https://burma-project-ideas.vercel.app/{bird.ImagePath}"
-                //};
                 var item = Change(bird);
                 return Ok(item);
             }
